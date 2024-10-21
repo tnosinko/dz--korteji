2a8570839916884e4bdf19966497974cd8f0d4f9
Татьяна <tnosinko@gmail.com>
2024-10-21 15:41:32 +0300

    Add files via upload

---
+++ module_1_5..py
@@ -0,0 +1,10 @@
+immutable_var = 123, "Phyton"
+print (immutable_var)
+
+mutable_list = ("бантик", "салфетка", "ручка","жевачка")
+print (mutable_list)
+
+mutable_list = (["бантик", "салфетка", "ручка","жевачка"])
+print(mutable_list [1])
+mutable_list [1]= ("персик")
+print(mutable_list)
