79ff0f45e1fc3f96f41b3a492d700b240d8d6660
Татьяна <tnosinko@gmail.com>
2024-11-07 18:25:42 +0300

    Update module_1_5..py

--- module_1_5..py
+++ module_1_5..py
@@ -1,8 +1,7 @@
 immutable_var = 123, "Phyton"
 print (immutable_var)
+#immutable_var[0] = "Something" (так как нет функции tuple к которой обращается кортеж или то что immutable_var[0] обращение к индексу 0, которого нет в списке ранее)
 
-mutable_list = ("бантик", "салфетка", "ручка","жевачка")
-print (mutable_list)
 
 mutable_list = (["бантик", "салфетка", "ручка","жевачка"])
 print(mutable_list [1])
